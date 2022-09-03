f7c53e87734773554f835f88f4e95975bed92148
heli0santos <helio.santos@gmail.com>
2022-09-02 20:43:49 -0400

    onclick

--- app/src/main/java/com/example/colecaodegibis/MainActivity.java
+++ app/src/main/java/com/example/colecaodegibis/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.colecaodegibis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void visualizar(View view){
+        Toast.makeText(this, "Revista aberta", Toast.LENGTH_SHORT).show();
+    }
+
+    public void adicionar(View view){
+
+    }
+
+    public void x(){
+
     }
 }
