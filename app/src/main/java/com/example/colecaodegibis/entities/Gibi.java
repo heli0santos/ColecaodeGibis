9af42262b427d25c6086c99ec7aa089e628ed9bb
heli0santos <helio.santos@gmail.com>
2022-09-16 19:52:48 -0400

    Segunda tela.

---
+++ app/src/main/java/com/example/colecaodegibis/entities/Gibi.java
@@ -0,0 +1,49 @@
+package com.example.colecaodegibis.entities;
+
+public class Gibi {
+    private long _id;
+    private String nome;
+    private String titulo;
+    private String numero;
+    private String editora;
+
+    public long get_id() {
+        return _id;
+    }
+
+    public void set_id(long _id) {
+        this._id = _id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+}
---
+++ app/src/main/java/com/example/colecaodegibis/interfaces/IGibiController.java
@@ -0,0 +1,12 @@
+package com.example.colecaodegibis.interfaces;
+
+import com.example.colecaodegibis.entities.Gibi;
+
+import java.util.List;
+
+public interface IGibiController {
+    void create(Gibi gibi);
+    List<Gibi> read();
+    Gibi update(long i, Gibi gibi);
+    void delete(long i);
+}
